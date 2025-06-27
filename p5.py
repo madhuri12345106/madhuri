7df2a2b627250e6e4283cc5c7746e3d6d4328260
madhuri12345106 <madhurijaiswal84192@gmail.com>
2025-06-27 08:36:45 -0700

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,13 @@
+# name="shradha"
+# age=25
+# price=25.99
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+name1="sk"
+name2="sk"
+name3='''sk'''
+print(name1)
+print(name2)
+print(name3)
